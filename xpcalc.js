d4d209091e2e1c6529e1bd3243b9f376463bc9fc
Stephen Hurd <shurd@sasktel.net>
2021-06-17 19:03:00 -0400

    Update for 8.x

--- xpcalc.js
+++ xpcalc.js
@@ -6,13 +6,23 @@ Hooks.on("renderCombatTracker", (tracker, html, data) => {
 	let totalXP = 0;
 	data.combat.combatants.forEach(combatant => {
 		let tok = combatant.token;
+		let dispo;
 
-		if (tok?.disposition !== undefined) {
-			switch (tok.disposition) {
-				case TOKEN_DISPOSITIONS.FRIENDLY:
+		/* Non-CONST and tok.disposition are 7.x... remove when appropriate */
+		const FRIENDLY = CONST?.TOKEN_DISPOSITIONS?.FRIENDLY === undefined ? TOKEN_DISPOSITIONS.FRIENDLY : CONST.TOKEN_DISPOSITIONS.FRIENDLY;
+		const HOSTILE = CONST?.TOKEN_DISPOSITIONS?.HOSTILE === undefined ? TOKEN_DISPOSITIONS.HOSTILE : CONST.TOKEN_DISPOSITIONS.HOSTILE;
+
+		if (tok?.disposition !== undefined)
+			dispo = tok.disposition;
+		else if (tok?.data?.disposition !== undefined)
+			dispo = tok.data.disposition;
+
+		if (dispo !== undefined) {
+			switch (dispo) {
+				case FRIENDLY:
 					allies++;
 					break;
-				case TOKEN_DISPOSITIONS.HOSTILE:
+				case HOSTILE:
 					if (combatant.actor?.data?.data?.details?.xp?.value !== undefined) {
 						let xp = combatant.actor.data.data.details.xp.value;
 						totalXP += xp;
